d2785159a44a393a338b322ab8e7f6132a7c2362
Camilo Castellar <camilocastelar3@gmail.com>
2022-05-22 18:11:20 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const botonEnviar = document.getElementById('submit');
+
+botonEnviar.addEventListener('click', function() {
+    window.location.href = 'https://www.freecodecamp.com/email-submit';
+});
